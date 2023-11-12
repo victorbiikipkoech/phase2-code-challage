8519cd656eba5a901a931262d4c81940da748f0b
VICTOR <victor.kipkoech@student.moringaschool.com>
2023-11-12 18:10:59 +0300

    updated

--- my-bank-app/src/App.js
+++ my-bank-app/src/App.js
@@ -10,36 +10,38 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    // Fetch transactions from the provided db.json file
-    fetch('/db.json')
-      .then((response) => response.json())
-      .then((data) => setTransactions(data.transactions))
-      .catch((error) => console.error('Error fetching data:', error));
+    // ... (previous code)
   }, []);
 
   useEffect(() => {
-    // Filter transactions based on the search term
-    setFilteredTransactions(
-      transactions.filter((transaction) =>
-        transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    );
+    // ... (previous code)
   }, [transactions, searchTerm]);
 
   const handleAddTransaction = (newTransaction) => {
-    // Add the new transaction to the local state (not persisted to the backend)
-    setTransactions([...transactions, newTransaction]);
+    // ... (previous code)
+  };
+
+  const handleDeleteTransaction = (id) => {
+    // Remove the transaction with the given id
+    const updatedTransactions = transactions.filter(
+      (transaction) => transaction.id !== id
+    );
+    setTransactions(updatedTransactions);
   };
 
   return (
     <div>
       <h1>My Bank App</h1>
       <SearchBar onSearch={setSearchTerm} />
-      <TransactionTable transactions={filteredTransactions} />
+      <TransactionTable
+        transactions={filteredTransactions}
+        onDeleteTransaction={handleDeleteTransaction}
+      />
       <TransactionForm onAddTransaction={handleAddTransaction} />
     </div>
   );
 };
 
 export default App;
+
 
