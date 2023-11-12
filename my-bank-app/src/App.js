b67f9df465f7cfa11015bd7151ce12c3ae6fc398
VICTOR <victor.kipkoech@student.moringaschool.com>
2023-11-12 22:41:54 +0300

    updated

--- my-bank-app/src/App.js
+++ my-bank-app/src/App.js
@@ -10,38 +10,35 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    // ... (previous code)
+    // Fetch transactions from the provided db.json file
+    fetch('/db.json')
+      .then((response) => response.json())
+      .then((data) => setTransactions(data.transactions))
+      .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
   useEffect(() => {
-    // ... (previous code)
+    // Filter transactions based on the search term
+    setFilteredTransactions(
+      transactions.filter((transaction) =>
+        transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
   }, [transactions, searchTerm]);
 
   const handleAddTransaction = (newTransaction) => {
-    // ... (previous code)
-  };
-
-  const handleDeleteTransaction = (id) => {
-    // Remove the transaction with the given id
-    const updatedTransactions = transactions.filter(
-      (transaction) => transaction.id !== id
-    );
-    setTransactions(updatedTransactions);
+    // Add the new transaction to the local state (not persisted to the backend)
+    setTransactions([...transactions, newTransaction]);
   };
 
   return (
     <div>
       <h1>My Bank App</h1>
       <SearchBar onSearch={setSearchTerm} />
-      <TransactionTable
-        transactions={filteredTransactions}
-        onDeleteTransaction={handleDeleteTransaction}
-      />
+      <TransactionTable transactions={filteredTransactions} />
       <TransactionForm onAddTransaction={handleAddTransaction} />
     </div>
   );
 };
 
 export default App;
-
-
--- my-bank-app/src/components/TransactionTable.js
+++ my-bank-app/src/components/TransactionTable.js
@@ -1,15 +1,19 @@
 // src/components/TransactionTable.js
-import React from 'react';
+import React, { useState } from 'react';
 
-const TransactionTable = ({ transactions }) => {
+const TransactionTable = ({ transactions, onDeleteTransaction }) => {
+  // ... (previous code)
+
+  const handleDelete = (id) => {
+    onDeleteTransaction(id);
+  };
+
   return (
     <table>
       <thead>
         <tr>
-          <th>Date</th>
-          <th>Description</th>
-          <th>Category</th>
-          <th>Amount</th>
+          {/* ... (previous code) */}
+          <th>Action</th>
         </tr>
       </thead>
       <tbody>
@@ -19,6 +23,11 @@ const TransactionTable = ({ transactions }) => {
             <td>{transaction.description}</td>
             <td>{transaction.category}</td>
             <td>{transaction.amount}</td>
+            <td>
+              <button onClick={() => handleDelete(transaction.id)}>
+                Delete
+              </button>
+            </td>
           </tr>
         ))}
       </tbody>
@@ -27,3 +36,6 @@ const TransactionTable = ({ transactions }) => {
 };
 
 export default TransactionTable;
+
+
+
